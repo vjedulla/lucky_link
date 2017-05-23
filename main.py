b9fb5717cf9a85670ee8a9f43b9455aab6b58116
Armando Alliu <armando@manoolia.com>
2017-05-23 12:55:16 +0200

    minor fix

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 from random import randint
 
 youtube = 'https://www.youtube.com'
-url = '/watch?v=xVPvzX-AeSM'
-lucky_digit = 55
+url = '/watch?v=YfFhxA2JFOA'
+lucky_digit = 2
+randomOrFirst = True
 
 out = open('test.txt', 'w')
 
@@ -21,16 +22,18 @@ for i in range(0, lucky_digit):
         out.write(my_str)
         t += 1
 
-    rand = randint(0, len(tags) - 1) # chose a random video ( fun results )
-    # rand = 0 # for next autoplay video
+    if randomOrFirst:
+        rand = randint(0, len(tags) - 1) # chose a random video ( fun results )
+    else:
+        rand = 0 # for next autoplay video
 
     url = tags[rand]['href']    # get the value of the next link
     curr = tags[rand]['title']  # get the title of that link
     curr_url = tags[rand]['href']  # get the title of that link
 
     out.write('\n-------- Chosen: ' + tags[rand]['title'].encode('ascii', 'ignore') + ' [' + str(rand) + ']--------\n\n')
-    # print str(i/float(lucky_digit )) + '%\n'    # percentage
-    print '\r[{0}] {1}%'.format('#'*(i/10), i)
+
+    print '\r[{0}] {1:.0f}%'.format('#'*(i/10), (((i + 1)**1.0)/lucky_digit) * 100)
 
 
 print '\n'
